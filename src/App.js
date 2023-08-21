b8f9fedc0f6c55217540396d21653b5530cdaacd
Damian <81990930+damiadev94@users.noreply.github.com>
2023-08-21 18:08:04 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MessageCard from "./MessageCard";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="mx-8">
+      <h1 className="font-extrabold my-8 text-[2rem]">React Hooks Testing</h1>
+      <MessageCard />
+    </div>
+  );
+}
